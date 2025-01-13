961e123c4d5ba761bfd98842dd9145bc7fa8679d
mohamedmagdi6 <mohamedmagdii137@gmail.com>
2025-01-13 13:27:09 +0200

    create Title section of onboarding screen

--- lib/features/onboarding/onboarding_screen.dart
+++ lib/features/onboarding/onboarding_screen.dart
@@ -1,11 +1,19 @@
 import 'package:doctor_app/features/onboarding/widgets/logo_and_app_title.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class OnboardingScreen extends StatelessWidget {
   const OnboardingScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Column(children: [LogoAndAppTitle()]));
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 30.h),
+          child: Column(children: [LogoAndAppTitle(), SizedBox(height: 40.h)]),
+        ),
+      ),
+    );
   }
 }
--- lib/features/onboarding/widgets/logo_and_app_title.dart
+++ lib/features/onboarding/widgets/logo_and_app_title.dart
@@ -1,12 +1,19 @@
+import 'package:doctor_app/core/resources/text_style_manager.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class LogoAndAppTitle extends StatelessWidget {
   const LogoAndAppTitle({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Row(children: [
-        
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        SvgPicture.asset('assets/svgs/logo.svg'),
+        SizedBox(width: 10.w),
+        Text('Docdoc', style: TextStyleManager.textstyleS24W700Black()),
       ],
     );
   }
